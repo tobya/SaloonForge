9f302649783629894ac189f00bee285ea4c09d70
Toby Allen <toby@cookingisfun.ie>
2026-03-25 07:28:24 +0000

    configure

---
+++ src/Commands/ForgeRequestCommand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tobya\SaloonForge\Commands;
+
+use Illuminate\Console\Command;
+
+class ForgeRequestCommand extends Command{
+    protected $signature = 'saloon:forgerequest';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        //
+    }
+}
