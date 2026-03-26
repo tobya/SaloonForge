d88b25bf654b1d6e55d71752b3fbcfd0f5c39b08
Toby Allen <toby@cookingisfun.ie>
2026-03-26 07:56:48 +0000

    more config

--- config/saloonforge.php
+++ config/saloonforge.php
@@ -6,6 +6,20 @@ return [
     'routes' => [
         'selector' => \Tobya\SaloonForge\Selectors\RouteSelector::class,
         'prefix' => ['/'],
+        'exclude' =>
+            [
+              'middleware' => ['web'],
+             'filter' =>        ['/'],
+             'unnamed' => true,
+            ],
+        'include' => [
+            'middleware' => ['web'],
+            'route-parameters' => [
+                'any' =>    ['{v4}'],
+                'all' => [],  // not implemented
+            ],
+        ]
+
         ]
 
 
