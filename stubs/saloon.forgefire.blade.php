2f38cca3722a47c186bbb89797186738d4a46c8f
Toby Allen <toby@cookingisfun.ie>
2026-08-07 10:43:47 +0100

    stub changes

--- stubs/saloon.forgefire.blade.php
+++ stubs/saloon.forgefire.blade.php
@@ -14,26 +14,53 @@ namespace App\Http\Integrations\{{$integration}};
 class {{$integration}}
 {
       protected {{$integration}}Connector $connector;
+     /**
+     * @var null | Request
+     */
+
+      private $shouldReturnRequest = false;
 
       public function __construct(  )
       {
             $this->connector = new {{$integration}}Connector();
       }
 
+
+      public function getRequest() : static
+      {
+          $this->shouldReturnRequest = true;
+          return $this;
+      }
+
+      public function send(Request $request ) : Response
+      {
+            return $this->connector->send($request);
+      }
+
     @foreach ($requests as $request)
       /**
         * {{$request->name}}
-        * @return Response
+        * @return Response | {{$request->name}}
         */
-        public function {{$request->name}}({{$request->parameterlist()}}) : Response
+        public function {{$request->name}}({{$request->parameterlist()}}) : Response | {{$request->name}}
         {
 
             $request = new {{$request->name}}({{$request->parameterlist()}});
-            return $this->connector->send($request);
+
+            // dont send request just return the request to caller.
+            if ($this->shouldReturnRequest){
+                return $request;
+            }
+
+            return $this->send($request);
 
         }
 
 
     @endforeach
+
+
+
+
 }
 
