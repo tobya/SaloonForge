fad77472db0e17ae5e556d6c59477cb386492b5f
Toby Allen <toby@cookingisfun.ie>
2026-09-11 22:33:03 +0100

    tests

---
+++ tests/ConfigServicePestTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+    use Tobya\SaloonForge\Services\ConfigService;
+
+    it('can retireve a value from the config file', function () {
+
+        $configService = new ConfigService('Default');
+        \Illuminate\Support\Facades\Config::set('saloonforge.integrations.Default.routes.include.middleware',['notWeb']);
+
+        $configValue = $configService->Config('routes.include.middleware');
+
+        expect($configValue)->toBeArray();
+        expect($configValue)->toEqual(['notWeb']);
+
+    });
+
+
+    it('can retireve a value from the config file not default', function () {
+
+        $configService = new ConfigService('Photo');
+        \Illuminate\Support\Facades\Config::set('saloonforge.integrations.Photo.routes.include.middleware',['notWeb2']);
+
+        $configValue = $configService->Config('routes.include.middleware');
+
+        expect($configValue)->toBeArray();
+        expect($configValue)->toEqual(['notWeb2']);
+
+    });
